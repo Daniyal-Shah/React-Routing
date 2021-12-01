5b11b1fe4cc117632296834833de9f0fbbdcc259
Daniyal-Shah <daniyalshah7862018@gmail.com>
2021-12-01 23:16:08 +0500

    moved to react-router-dom V6

--- src/index.js
+++ src/index.js
@@ -3,15 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter}  from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route}  from "react-router-dom";
+import Products from './components/products';
+import Dashboard from './components/admin/dashboard';
+import Posts from './components/posts';
+
+
 ReactDOM.render(
-  <BrowserRouter>
-      <App />
-  </BrowserRouter>
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/products" element={<Products/>} />
+      <Route path="/admin" element={<Dashboard/>}  />
+      <Route path="/posts" element={<Posts/>}  />
+    </Routes>
+  </Router>
+  
   ,document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+function Home()
+{
+  return <div>
+    <h1>
+      home
+    </h1>
+  </div>
+}
+
 reportWebVitals();
