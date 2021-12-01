a47141787d9555c61c4396691508190e2284c3f8
Daniyal-Shah <daniyalshah7862018@gmail.com>
2021-12-01 23:45:35 +0500

    Link and Outlet

--- src/index.js
+++ src/index.js
@@ -1,27 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router, Routes, Route}  from "react-router-dom";
 import Products from './components/products';
 import Dashboard from './components/admin/dashboard';
 import Posts from './components/posts';
+import {BrowserRouter as Router, Routes, Route, Link, Navigate, Outlet}  from "react-router-dom";
 
 
 ReactDOM.render(
   <Router>
     <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/products" element={<Products/>} />
+
+      {/* <Route path="/products" element={<Products/>} />
       <Route path="/admin" element={<Dashboard/>}  />
-      <Route path="/posts" element={<Posts/>}  />
+      <Route path="/posts" element={<Posts/>}  /> */}
+      
+      <Route path="/" element={<Home/>} />
+
+      <Route path="learn" element={<Learn/>} >
+        <Route path="courses" element={<Courses/>}  />
+        <Route path="bundles" element={<Bundles/>}  />
+      </Route>
+
+  
+
     </Routes>
   </Router>
-  
+
   ,document.getElementById('root')
 );
 
+
+function Learn()
+{
+  return( 
+    <div>
+          <h1 className="mb-4">React Routing Practice</h1>
+          <Link to="/learn/courses" className="btn btn-success m-2">Courses</Link>
+          <Link to="/learn/bundles" className="btn btn-primary m-2">Bundles</Link>
+          <Outlet />
+    </div>)
+
+}
 
 
 function Home()
@@ -32,5 +55,24 @@ function Home()
     </h1>
   </div>
 }
+
+function Courses()
+{
+  return <div>
+    <h1>
+      Courses
+    </h1>
+  </div>
+}
+
+function Bundles()
+{
+  return <div>
+    <h1>
+    Bundles
+    </h1>
+  </div>
+}
+
 
 reportWebVitals();
